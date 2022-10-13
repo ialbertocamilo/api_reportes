4569e926c7b02a1de643d5ddb38e9eb2f66d66a7
deyvijames <deyvi@lamediadl.com>
2022-10-12 19:31:28 -0500

    Limpieza variables entorno test

--- cron.js
+++ cron.js
@@ -1,28 +1,27 @@
-const path = require('path')
-const fs = require('fs')
-const cron = require('node-cron')
-const { CARPETA_DESCARGA, CARPETA_DESCARGA_TEST } = require('./config')
+const path = require("path");
+const fs = require("fs");
+const cron = require("node-cron");
+const { CARPETA_DESCARGA } = require("./config");
 // Cron schedule = sec | min | hour | day | month | dayofweek
 
-cron.schedule('0 0 3 * * *', () => {
-  borrarArchivos(CARPETA_DESCARGA)
-  borrarArchivos(CARPETA_DESCARGA_TEST)
-})
+cron.schedule("0 0 3 * * *", () => {
+  borrarArchivos(CARPETA_DESCARGA);
+});
 
-function borrarArchivos (_path) {
+function borrarArchivos(_path) {
   fs.readdir(_path, function (err, files) {
-    if (err) return console.log('No se puede escanear el directorio: ' + err)
+    if (err) return console.log("No se puede escanear el directorio: " + err);
 
     files.forEach(function (file) {
-      const extension = path.extname(file)
-      if (extension == '.xlsx' || extension == '.csv') {
-        fs.unlink(_path + file, err => {
+      const extension = path.extname(file);
+      if (extension == ".xlsx" || extension == ".csv") {
+        fs.unlink(_path + file, (err) => {
           if (!err) {
-            console.log('El Archivo : ' + file + ' se borro.')
+            console.log("El Archivo : " + file + " se borro.");
           }
-        })
+        });
       }
-    })
-    console.log('Termino el proceos de borrado')
-  })
+    });
+    console.log("Termino el proceos de borrado");
+  });
 }
