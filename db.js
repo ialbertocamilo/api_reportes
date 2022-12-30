886a435ee72776c06f0b307435ee89e4744481d5
Aldo Lopez <aldo@lamediadl.com>
2022-12-30 17:45:55 -0500

    Se añadio puerto a la base de datos

--- db.js
+++ db.js
@@ -4,6 +4,7 @@ exports.con = require('knex')({
   client: 'mysql2',
   version: '5.7',
   connection: {
+    port: config.DB_PORT,
     host: config.HOST,
     user: config.USER,
     password: config.PASSWORD,
