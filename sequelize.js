e9940f35e2a3d048f1e63ba7ece04dba8bb75c6e
Aldo Lopez <aldo@lamediadl.com>
2023-01-04 14:11:08 -0500

    Set port in connection to database with sequelize

--- sequelize.js
+++ sequelize.js
@@ -3,7 +3,8 @@ const config = require('./config')
 
 const sequelize = new Sequelize(config.DATABASE, config.USER, config.PASSWORD, {
     host: config.HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    port: config.DB_PORT, 
 });
 
 module.exports = sequelize
