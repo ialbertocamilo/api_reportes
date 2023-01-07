fdfab0123eb1bab121fcfc3a0fd7fe88592d339b
Elvis Fernandez <elvis@cursalab.io>
2023-01-07 12:17:22 -0500

    Route has been changed for supervisores_notas

--- routes/routes.route.js
+++ routes/routes.route.js
@@ -26,8 +26,9 @@ router.post('/temas_no_evaluables_v2', handler.temasNoEvaluablesV2)
 router.post('/ranking', handler.ranking)
 router.post('/user_uploads', handler.userUploads)
 
-router.post('/supervisores_notas', handler.supervisores_notas)
-router.post('/supervisores_notas2', handler.supervisores_notas2)
+router.post('/supervisores_notas', handler.supervisores_notas2)
+//router.post('/supervisores_notas', handler.supervisores_notas)
+
 router.post('/supervisores_avance_curricula', handler.supervisores_avance_curricula)
 
 router.post('/checklist_general', handler.checklist_general)
