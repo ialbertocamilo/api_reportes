7de994e6598a13d9268eee5766862dcc558a34a1
Elvis Fernandez <elvis@cursalab.io>
2023-02-20 14:16:14 -0500

    Bug fixes

--- helper/Workspace.js
+++ helper/Workspace.js
@@ -3,6 +3,7 @@ const { pluck } = require('./Helper')
 
 exports.getSuboworkspacesIds = async (workspaceId) => {
   const subworkspaces = await con('workspaces')
+    .where('active', 1)
     .where('parent_id', workspaceId)
 
   return pluck(subworkspaces, 'id')
