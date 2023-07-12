5d5d8f0d71cbffa278bd8b8751f02f21f7f23c30
Elvis Fernandez <elvis@cursalab.io>
2023-07-12 13:37:23 -0500

    Elvis: the way in which segmented courses are loaded has been fixed

--- helper/Courses.js
+++ helper/Courses.js
@@ -75,10 +75,6 @@ exports.calculateSchoolProgressPercentage = (
       +us.user_id === +userId
   })
 
-  if (userId === 87365) {
-    console.log(usersCoursesProgress, userId, schoolId, userSegmentedSchoolsCourses)
-  }
-
   if (!schoolInfo) return 0
 
   let progressSum = 0;
