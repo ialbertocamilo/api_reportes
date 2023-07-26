36748feb998aa1b2b3c2b6f47756b2075c60261d
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-07-26 12:55:09 -0500

    adrian: download file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 const queue = require('express-queue')
 const cors = require('cors')
@@ -13,7 +14,6 @@ const { restartQueueExecution } = require('./helper/Queue')
 const moment = require('moment-timezone')
 const { downloadFile } = require('./s3/storage')
 // Server config
-
 // app.use(queue({ activeLimit: 10, queuedLimit: -1 }))
 app.use(cors())
 app.use(express.json())
@@ -74,11 +74,20 @@ app.post('/reports/queue/started/:workspaceId', async (req, res) => {
   const protocol = process.env.IS_LOCALHOST == 1 ? 'http' : 'https'
   res.json({ started: await restartQueueExecution(io, req.body.adminId, req.params.workspaceId, protocol + '://' + req.headers.host) })
 })
-app.get('/reports/:filename',async (req, res) => {
-   try {
-    await downloadFile(req.params.filename, res); // Pasamos 'res' como segundo argumento
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Error al procesar la solicitud." });
-  }
+
+app.get('/reports/:filename', async (req, res) => {  
+  const urlArchivoAmazon = await downloadFile(req.params.filename)
+  try {
+      const response = await axios({
+        method: 'GET',
+        url: urlArchivoAmazon,
+        responseType: 'stream',
+      });
+
+      res.setHeader('Content-Disposition', `attachment; filename="${req.params.filename}"`);
+      response.data.pipe(res);
+    } catch (error) {
+      console.error('Error en la solicitud:', error.message);
+      res.status(500).send('Error al descargar el archivo.');
+    }
 })
--- s3/storage.js
+++ s3/storage.js
@@ -1,15 +1,14 @@
 const fs = require('fs')
-const path = require('path')
 
 const { AWS_BUCKET_NAME, MARCA } = require('../config.js')
-const { PutObjectCommand,GetObjectCommand } = require('@aws-sdk/client-s3')
+const { PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3')
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 
 const { client } = require('../helper/s3-helpers.js')
-
-const uploadFile =async (filePath) => {
+const uploadFile = async (filePath) => {
   try {
-    const fileStream = fs.createReadStream('./'+filePath)
-    const keyFile = MARCA  + fileStream.path.slice(1)
+    const fileStream = fs.createReadStream('./' + filePath)
+    const keyFile = MARCA + fileStream.path.slice(1)
     const compressedCommand = new PutObjectCommand({
       Bucket: AWS_BUCKET_NAME,
       Key: keyFile,
@@ -22,22 +21,21 @@ const uploadFile =async (filePath) => {
     console.error(err)
   }
 }
-const downloadFile = async (filePath, res) => {
-  try {
-    const keyFile = `${MARCA}/reports/${filePath}`
-    const params = {
+
+async function downloadFile(filePath) {
+  try{
+    const expiresIn = 60 * 5
+  
+    const command = new GetObjectCommand({
       Bucket: AWS_BUCKET_NAME,
-      Key: keyFile
-    }
-    const command = new GetObjectCommand(params)
-    const result = await client.send(command)
-
-    res.send(result.Body);
-
+      Key: `${MARCA}/reports/${filePath}`,
+    })
+    const url = await getSignedUrl(client, command, { expiresIn })
+    return url
   } catch (err) {
-    console.log(err);
-    res.status(500).json({ error: "Error al descargar el archivo." });
-  }
+    console.log(err)
+  }  
+  
 }
 
-module.exports = { uploadFile, downloadFile  }
+module.exports = { uploadFile, downloadFile }
