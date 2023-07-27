a03eb0ec78b0c81acb7230762fb60230d7eb0d60
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-07-27 10:47:45 -0500

    adrian: fixes directorio

--- response.js
+++ response.js
@@ -1,13 +1,15 @@
 const { extension } = require('./config')
 const moment = require('moment')
 const { generateReportPath } = require('./helper/Queue')
+const {downloadFile} = require('./s3/storage')
+const { extension } = require('./config')
 
 // moment('es')
 exports.response = (responseData) => {
   const { createAt, modulo, error, alert } = responseData
   if (modulo) {
     return {
-      ruta_descarga: generateReportPath(createAt),
+      ruta_descarga: downloadFile(createAt+extension),
       createAt,
       modulo,
       new_name: `${modulo}_${moment(createAt)
