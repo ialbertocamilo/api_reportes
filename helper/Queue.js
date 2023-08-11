4eed182fec3ba302c546f73d5ff552f1b63da6ae
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-08-11 15:46:42 -0500

    logs

--- helper/Queue.js
+++ helper/Queue.js
@@ -90,7 +90,7 @@ exports.markReportAsReady = async (
       is_ready: false
     }
   })
-
+  console.log(stringify(reports))
   if (reports[0]) {
     reports[0].is_ready = 1
     reports[0].is_processing = 0
