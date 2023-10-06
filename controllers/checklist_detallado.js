bcd085443cc96964c7bf7816c6ead1fbfa70805b
Elvis Fernandez <elvis@cursalab.io>
2023-10-06 14:03:54 -0500

    Override columns of users without answers in the provided checklist

--- controllers/checklist_detallado.js
+++ controllers/checklist_detallado.js
@@ -224,11 +224,16 @@ left join courses c on
   // `
   //a checklist could be associated with one or more courses
   let staticCondition = ` where 
-         u.id in (${userIdsSegmentedToChecklist.join(',')})
-         and (
-            ca.checklist_id in (${Array.isArray(checklistId) ? checklistId.join(',') : checklistId})
-            or
-            ca.checklist_id is null
+         (
+           u.id in (${userIdsSegmentedToChecklist.join(',')})
+           and (
+              ca.checklist_id in (${Array.isArray(checklistId) ? checklistId.join(',') : checklistId})
+              or
+              ca.checklist_id is null
+           )
+         )
+         or (
+          u.id in (${userIdsSegmentedToChecklist.join(',')})
          )
           `
 
