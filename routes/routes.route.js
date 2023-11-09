3772c6ac4292695606b0be07888327dfffeba7c5
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-11-09 12:15:11 -0500

    rutadescarga

--- routes/routes.route.js
+++ routes/routes.route.js
@@ -123,11 +123,12 @@ const reportFinishedHandler = async (protocol, headers, children, io, reportType
   if (rutaDescarga) {
     message = `Tu reporte "${reportName}" se encuentra listo.`
     success = true
+    const partes = rutaDescarga.split('/');
+    const nombreArchivo = partes[partes.length - 1];
+    const urlS3 = await downloadFile(nombreArchivo)
+    console.log(urlS3,'urlDownload')
   }
-  const partes = rutaDescarga.split('/');
-  const nombreArchivo = partes[partes.length - 1];
-  const urlS3 = await downloadFile(nombreArchivo)
-  console.log(urlS3,'urlDownload')
+
   console.log('Notify user: report-finished')
   io.sockets.emit('report-finished', {
     adminId: body.adminId,
