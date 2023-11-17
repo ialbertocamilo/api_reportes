d4330d32c3f17bcb96101e8e2cdf9cc4b78d2273
AdrianEsqueiros <u201718169@upc.edu.pe>
2023-11-17 17:16:51 -0500

    report commit

--- sequelize.js
+++ sequelize.js
@@ -2,17 +2,17 @@ const { Sequelize } = require('sequelize')
 const config = require('./config')
 
 const sequelize = new Sequelize(config.DATABASE, config.USER, config.PASSWORD, {  
+  host: config.HOST,
   dialect: 'mysql',
   port: config.DB_PORT,
   logging: false,
   timezone: '-05:00',
-  replication: {
-    read: [
-      { host: config.HOST, username: config.USER,  password:config.PASSWORD}      
-    ],
-    write: { host: config.HOST_WRITE, username: config.USER,  password:config.PASSWORD }
-    
-  },
+  // replication: {
+  //   read: [
+  //     { host: config.HOST, username: config.USER,  password:config.PASSWORD}      
+  //   ],
+  //   write: { host: config.HOST_WRITE, username: config.USER,  password:config.PASSWORD }    
+  // },
 
 })
 
